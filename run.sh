9caef384ca8b00ccd90ee936b4442de201b683ed
lixining <116860773@qq.com>
2021-10-24 14:54:33 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "检查更新"
+git -C /xuexi/code/TechXueXi pull $Sourcepath $pullbranche
+echo "检查更新完毕"
+cp -r /xuexi/code/TechXueXi/SourcePackages/* /xuexi
+echo "覆盖自主代码"
+cp -r /xuexi/mycode/* /xuexi/pdlearn
+echo "开始运行"
+/usr/local/bin/python /xuexi/pandalearning.py
